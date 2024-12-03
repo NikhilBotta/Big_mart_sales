93db6d80a2cb53612e457b8a69e1f45ac048e5f4
NIKHILBOTTA <nikhilbotta@gmail.com>
2024-12-04 03:12:35 +0530

    Data Ingestion file is completed

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from big_mart_sales.components.data_ingestion import DataIngestion
+from big_mart_sales.components.data_validation import DataValidation
 from big_mart_sales.exception.exception import BigMartSalesException
 from big_mart_sales.logging.logger import logging
-from big_mart_sales.entity.config_entity import DataIngestionConfig 
+from big_mart_sales.entity.config_entity import DataIngestionConfig , DataValidationconfig
 from big_mart_sales.entity.config_entity import TrainingPipelineConfig
 import sys
 
@@ -15,5 +16,14 @@ if __name__=='__main__':
         logging.info('Initiating data ingestion')
         dataingestionartifact=data_ingestion.initiate_data_ingestion()
         print(dataingestionartifact)
+        logging.info('Data Ingestion is completed')
+        data_validation_config=DataValidationconfig(trainingpipelineconfig)
+        data_validation=DataValidation(dataingestionartifact,data_validation_config)
+        logging.info('Initiating data ingestion')
+        data_validation_artifacts=data_validation.initiate_data_validation()
+        logging.info('Data validation completed')
+        print(data_validation_artifacts)
+
+        
     except Exception as e:
         raise BigMartSalesException(e,sys)
